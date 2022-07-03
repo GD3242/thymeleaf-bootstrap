5a5f2f5695af0ba692f157d411fa444b00761e6e
GD3242 <61698354+GD3242@users.noreply.github.com>
2022-07-03 22:26:35 +0800

    Initial commit

---
+++ src/main/java/mn/dulguun/thymeleafbootsrap/ThymeleafBootsrapApplication.java
@@ -0,0 +1,13 @@
+package mn.dulguun.thymeleafbootsrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafBootsrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafBootsrapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mn/dulguun/thymeleafbootsrap/ThymeleafBootsrapApplicationTests.java
@@ -0,0 +1,13 @@
+package mn.dulguun.thymeleafbootsrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafBootsrapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
